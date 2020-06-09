c4cb0def93b8f0a6ca26b28f55e2b539369d0684
Fernando10g97 <luis.guerra@pucese.edu.ec>
2020-06-08 19:52:59 -0500

    Actualización de vistas con Node.js

--- src/app/routes/view3.js
+++ src/app/routes/view3.js
@@ -5,7 +5,7 @@ module.exports = app => {
   const connection = dbConnection();
 
   app.get('/view3', (req, res) => {
-    connection.query('SELECT * FROM ReporteTarjeta', (err, result) => {
+    connection.query('SELECT * FROM ReporteArtista', (err, result) => {
       res.render('views/view3', {
         view3: result
       });
